c0c9a95702be155d1ae798fb21040f88ca5a8922
AIboy996 <2207854887@qq.com>
2023-07-23 07:24:38 +0800

    fix color change

--- docs/assets/javascripts/color.js
+++ docs/assets/javascripts/color.js
@@ -3,22 +3,17 @@
     var day=date.getDay();
     var schemes=new Array("","mon","tue","wed","thur","fri","sat","sun");
     var colors=new Array("","grey","blue grey","teal","indigo","pink","deep purple","deep purple");
-    if (document.body.getAttribute("data-md-color-scheme") != schemes[day]){
-        // 更改页面主题
-        document.body.setAttribute("data-md-color-scheme",schemes[day]);
-        var local_json = {
-            'index': day-1,
-            'color': {
-                "scheme":schemes[day],
-                "primary":colors[day],
-                "accent":"orange"
-            }
-        };
-        // 把更改存储到本地
-        window.localStorage.setItem('/.__palette', JSON.stringify(local_json));
-        // 刷新页面
-        location.reload();
-    } else{
-        console.log("1");
+    document.body.setAttribute("data-md-color-scheme",schemes[day]);
+    var local_json = {
+        'index': day-1,
+        'color': {
+            "scheme":schemes[day],
+            "primary":colors[day],
+            "accent":"orange"
+        }
     };
+    // 把更改存储到本地
+    window.localStorage.setItem('/.__palette', JSON.stringify(local_json));
+    // 刷新页面
+    location.reload();
 }());
