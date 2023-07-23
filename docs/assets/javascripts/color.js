48cd9515a646d55a7482443ecf6812f518750f12
AIboy996 <2207854887@qq.com>
2023-07-23 12:55:32 +0800

    修复颜色变化的bug【周日的index为0】

--- docs/assets/javascripts/color.js
+++ docs/assets/javascripts/color.js
@@ -1,19 +1,24 @@
-(function changeColorByWeekday(){
+function changeColorByWeekday(){
     var date=new Date();
+    // 0-6代表周日到周六
     var day=date.getDay();
-    var schemes=new Array("","mon","tue","wed","thur","fri","sat","sun");
-    var colors=new Array("","grey","blue grey","teal","indigo","pink","deep purple","deep purple");
-    document.body.setAttribute("data-md-color-scheme",schemes[day]);
-    var local_json = {
-        'index': day-1,
-        'color': {
-            "scheme":schemes[day],
-            "primary":colors[day],
-            "accent":"orange"
-        }
-    };
-    // 把更改存储到本地
-    window.localStorage.setItem('/.__palette', JSON.stringify(local_json));
-    // 刷新页面
-    location.reload();
-}());
+    var schemes=new Array("sun","mon","tue","wed","thur","fri","sat",);
+    var colors=new Array("deep purple","grey","blue grey","teal","indigo","pink","deep purple");
+    if (document.body.getAttribute("data-md-color-scheme") != schemes[day]) {
+        document.body.setAttribute("data-md-color-scheme", schemes[day]);
+        var local_json = {
+            'index': day,
+            'color': {
+                "scheme":schemes[day],
+                "primary":colors[day],
+                "accent":"orange"
+            }
+        };
+        // 把更改存储到本地
+        window.localStorage.setItem('/.__palette', JSON.stringify(local_json));
+        // 刷新页面
+        location.reload();
+    }
+}
+
+window.addEventListener("load", changeColorByWeekday);
