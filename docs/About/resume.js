44d556081e9bd7961784e355e64c64ca1eb1d9d3
AIboy996 <2207854887@qq.com>
2023-09-12 12:56:24 +0800

    fix m.resume

--- docs/About/resume.js
+++ docs/About/resume.js
@@ -3,10 +3,3 @@ function myFunction() {
     var popup = document.getElementById("myPopup");
     popup.classList.toggle("show");
 }
-var width = ((typeof screen !== 'undefined') ? screen.width : null)
-    || window.innerWidth
-    || document.documentElement.clientWidth
-    || document.body.clientWidth;
-if (width <= 800) {
-    window.location = "m.resume.html";
-}
