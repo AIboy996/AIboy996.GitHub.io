79ba8b06a87d24f977a19c9ffbf0b68691c9d290
AIboy996 <2207854887@qq.com>
2024-05-21 21:18:23 +0800

    [update] color change js

--- docs/assets/javascripts/color.js
+++ docs/assets/javascripts/color.js
@@ -3,7 +3,7 @@ function changeColorByWeekday() {
     // 0-6代表周日到周六
     var day = date.getDay() + 1;
     var schemes = new Array("placeholder", "sun", "mon", "tues", "wed", "thur", "fri", "sat");
-    var colors = new Array("white", "purple", "grey", "blue grey", "teal", "indigo", "pink", "deep-purple");
+    var colors = new Array("white", "purple", "grey", "blue-grey", "teal", "indigo", "pink", "deep-purple");
     if (document.body.getAttribute("data-md-color-scheme") != schemes[day]) {
         document.body.setAttribute("data-md-color-scheme", schemes[day]);
         document.body.setAttribute("data-md-color-primary", colors[day]);
