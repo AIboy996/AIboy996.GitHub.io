525067d13013e0bfb211a7942bd53b853b1e5d5a
AIboy996 <2207854887@qq.com>
2024-07-09 02:31:09 +0800

    [update] giscus

--- docs/assets/javascripts/link.js
+++ docs/assets/javascripts/link.js
@@ -1,3 +1,7 @@
-document.querySelectorAll(".md-content a[href *='//']:not(.md-content__button):not(:has(img))").forEach(function(link) {
-    link.setAttribute('target', '_blank');
-});
+function link_blank() {
+    document.querySelectorAll(".md-content a[href *='//']:not(.md-content__button):not(:has(img))").forEach(function (link) {
+        link.setAttribute('target', '_blank');
+    });
+};
+
+window.addEventListener("load", link_blank);
