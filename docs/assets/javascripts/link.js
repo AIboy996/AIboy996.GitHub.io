31ae882e3c253ee1f806722cc114a7c2e8c83a46
AIboy996 <2207854887@qq.com>
2024-08-02 23:34:16 +0800

    [feature] img lazy loading

--- docs/assets/javascripts/link.js
+++ docs/assets/javascripts/link.js
@@ -2,6 +2,9 @@ function link_blank() {
     document.querySelectorAll(".md-content a[href *='//']:not(.md-content__button):not(:has(img))").forEach(function (link) {
         link.setAttribute('target', '_blank');
     });
+    document.querySelectorAll("figure img").forEach(function (img) {
+        img.setAttribute('loading', 'lazy');
+    });
 };
 
 window.addEventListener("load", link_blank);
