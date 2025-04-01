f98e7a22d61c99a5d926d09d29cd3e73c903f824
AIboy996 <2207854887@qq.com>
2025-04-01 15:55:17 +0800

    [feature] mkdocs-cust

--- docs/assets/javascripts/link.js
+++
@@ -1,7 +0,0 @@
-function link_blank() {
-    document.querySelectorAll(".md-content a[href *='//']:not(.md-content__button):not(:has(img))").forEach(function (link) {
-        link.setAttribute('target', '_blank');
-    });
-};
-
-window.addEventListener("load", link_blank);
